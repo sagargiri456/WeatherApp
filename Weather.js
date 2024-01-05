1ea1876009f6581a9c8ec401bf230aa39c8d35c2
sagargiri456 <sagargiri456@gmail.com>
2024-01-05 05:31:37 -0800

    vercel ko baps laya aur cors ko v

--- Weather.js
+++ Weather.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const cors = require("cor");
+const cors = require("cor");
 const app = express();
 const bodyParser = require("body-parser");
 const https = require("https");
@@ -9,13 +9,13 @@ const port = process.env.PORT || 5000
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
-// app.use(cors(
-//   {
-// origin: ["https://weather-app-phi-six-51.vercel.app/"],
-// method:["POST","GET"],
-// credentials:true
-//   }
-// ));
+app.use(cors(
+  {
+origin: ["https://weather-app-phi-six-51.vercel.app/"],
+method:["POST","GET"],
+credentials:true
+  }
+));
 app.get("/", (req, res) => {
   const cityimageurl = "citypng.png";
   const heading = "Weather App";
